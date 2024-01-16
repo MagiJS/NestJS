ac79eb5534ab87766cec4cf8aea3413b9d502aa5
tkgstrator <nasawake.am@gmail.com>
2024-01-16 20:33:22 +0900

    use fastify, dayjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env", ".env.local", ".env.production"],
+      isGlobal: true,
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,98 @@
-import { ValidationPipe } from "@nestjs/common";
+import { LogLevel, ValidationPipe, VersioningType } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
-import { SwaggerModule, DocumentBuilder, OpenAPIObject } from "@nestjs/swagger";
-import { AppModule } from "./app.module";
-import { config } from "dotenv";
-import { exec } from "child_process";
-import { mkdir, writeFileSync } from "fs";
-import * as path from "path";
-config({ path: ".env" });
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import fastify from "fastify";
 
-async function build(documents: OpenAPIObject) {
-  const build = path.resolve(process.cwd(), "docs");
-  const output = path.resolve(build, "index");
-  mkdir(build, { recursive: true }, () => {});
-  writeFileSync(`${output}.json`, JSON.stringify(documents), {
-    encoding: "utf8",
-  });
-  exec(`npx redoc-cli build ${output}.json -o ${output}.html`);
-}
+import content from "../package.json";
+
+import { AppModule } from "./app.module";
+import { onSend, preValidation } from "./fastify/log";
+import { ceil } from "./utils/helper";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const server = fastify({ bodyLimit: 50 * 1024 * 1024 });
+  const logLevels: LogLevel[] = isDevelopment
+    ? ["log", "error", "warn", "debug", "verbose"]
+    : ["log", "error", "warn"];
+  if (isDevelopment) {
+    server.addHook("preValidation", preValidation);
+    server.addHook("onSend", onSend);
+    server.addHook("onRequest", (request, reply, done) => {
+      const replyUnknown = reply as any;
+      replyUnknown["setHeader"] = reply.header.bind(reply);
+      replyUnknown["end"] = reply.send.bind(reply);
+      done();
+    });
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const adapter = new FastifyAdapter(server);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, adapter, {
+    logger: logLevels,
+  });
+
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.extend(ceil);
+  dayjs.tz.setDefault("Asia/Tokyo");
+
+  app.enableVersioning({ defaultVersion: "1", type: VersioningType.URI });
+  app.enableCors({
+    allowedHeaders:
+      "X-Web-View-Ver, X-GameWebToken, X-NaCountry, X-ProductVersion, X-Platform, X-Znca-Platform, X-Znca-Version, Origin, Content-Type, Accept, Authorization",
+    credentials: false,
+    maxAge: 86400,
+    optionsSuccessStatus: 200,
+    origin: "*",
+    preflightContinue: false,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
-      disableErrorMessages: true,
+      disableErrorMessages: !isDevelopment,
       transform: true,
+      transformOptions: {
+        excludeExtraneousValues: true,
+        exposeDefaultValues: false,
+        exposeUnsetFields: false,
+        ignoreDecorators: false,
+      },
+      validateCustomDecorators: true,
     }),
   );
-  const options = new DocumentBuilder().build();
-  const documents = SwaggerModule.createDocument(app, options);
-  build(documents);
-  SwaggerModule.createDocument;
-  SwaggerModule.setup("documents", app, documents);
-  await app.listen(process.env.PORT || 3000);
+
+  const config = app.get(ConfigService);
+  const configuration = {
+    host: config.get<string>("API_HOST"),
+    port: parseInt(config.get<string>("API_PORT"), 10),
+    version: content.version ?? "0.0.0",
+  };
+  if (configuration.version === undefined) {
+    throw new Error("API_VERSION is not defined.");
+  }
+  if (configuration.host === undefined) {
+    throw new Error("API_HOST is not defined.");
+  }
+  if (configuration.port === undefined) {
+    throw new Error("API_PORT is not defined.");
+  }
+
+  if (isDevelopment) {
+    const documentConfig = new DocumentBuilder()
+      .setTitle(content.name)
+      .setDescription(content.description)
+      .setVersion(content.version)
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, documentConfig);
+    SwaggerModule.setup("docs", app, document);
+  }
+  await app.listen(configuration.port || 3000, configuration.host || "0.0.0.0");
 }
 bootstrap();
